e0d90be7a2d925d26b75f1ff78a24035a20f905e
Muhammad Farhan Juna <62230384+mjuna12@users.noreply.github.com>
2022-01-28 17:04:06 +0700

    Update add_kategori.php

--- admin/pages/tables/kategori/process/add_kategori.php
+++ admin/pages/tables/kategori/process/add_kategori.php
@@ -3,8 +3,8 @@
     include '../../../../../koneksi/connection.php';
 
     // mendapatkan nilai dari form
-    $id_kategori     = $_POST['id_kategori'];
-    $nama_kategori    = $_POST['nama_kategori'];
+    $id_kategori     = mysql_escape_string($_POST['id_kategori']);
+    $nama_kategori    = mysql_escape_string($_POST['nama_kategori']);
 
     $query = "INSERT INTO kategori (id_kategori, nama_kategori, deleted) VALUES ('$id_kategori','$nama_kategori',0)";
 
