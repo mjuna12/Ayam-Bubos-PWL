4c549a247ed33f0a32dd71bd6ec8dc002c8cce62
Muhammad Farhan Juna <62230384+mjuna12@users.noreply.github.com>
2022-01-28 15:58:59 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<title>Cart</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="Sublime project">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-	<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="styles/cart.css">
-	<link href='images/logo.png' rel='SHORTCUT ICON' />
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
-	 crossorigin="anonymous">
-</head>
+<?php include("partials/headercart.php"); ?>
 
 <?php 
 include 'koneksi/connection.php';
