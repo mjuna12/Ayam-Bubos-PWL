5a8f2a0db41044e303d0cf05296cbb56a444474a
Muhammad Farhan Juna <62230384+mjuna12@users.noreply.github.com>
2022-01-28 17:04:24 +0700

    Update edit_kategori.php

--- admin/pages/tables/kategori/process/edit_kategori.php
+++ admin/pages/tables/kategori/process/edit_kategori.php
@@ -3,8 +3,8 @@
     include '../../../../../koneksi/connection.php';
 
     // mendapatkan nilai dari form
-    $id_kategori     = $_POST['id_kategori'];
-    $nama_kategori    = $_POST['nama_kategori'];
+    $id_kategori     = mysql_escape_string($_POST['id_kategori']);
+    $nama_kategori    = mysql_escape_string($_POST['nama_kategori']);
 
     $query = "UPDATE kategori SET nama_kategori = '$nama_kategori' WHERE id_kategori = '$id_kategori'";
 
