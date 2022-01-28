1613efee4d68052fa20ec2c0da21846e89aba3b8
Muhammad Farhan Juna <62230384+mjuna12@users.noreply.github.com>
2022-01-28 16:01:37 +0700

    Create headercategories.php

---
+++ partials/headercategories.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Categories</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Sublime project">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+	<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="styles/categories.css">
+</head>
