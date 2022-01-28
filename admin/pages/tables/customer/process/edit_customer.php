4913606a30ab060445cb768bc404aef1619f25ea
Muhammad Farhan Juna <62230384+mjuna12@users.noreply.github.com>
2022-01-28 17:03:33 +0700

    Update edit_customer.php

--- admin/pages/tables/customer/process/edit_customer.php
+++ admin/pages/tables/customer/process/edit_customer.php
@@ -3,12 +3,12 @@
     include '../../../../../koneksi/connection.php';
 
     // mendapatkan nilai dari form
-    $id_customer     = $_POST['id_customer'];
-    $nama_customer    = $_POST['nama_customer'];
-    $jk_customer    = $_POST['jk_customer'];
-    $alamat_customer    = $_POST['alamat_customer'];
-    $email_customer    = $_POST['email_customer'];
-    $telp_customer    = $_POST['telp_customer'];
+    $id_customer     = mysql_escape_string($_POST['id_customer']);
+    $nama_customer    = mysql_escape_string($_POST['nama_customer']);
+    $jk_customer    = mysql_escape_string($_POST['jk_customer']);
+    $alamat_customer    = mysql_escape_string($_POST['alamat_customer']);
+    $email_customer    = mysql_escape_string($_POST['email_customer']);
+    $telp_customer    = mysql_escape_string($_POST['telp_customer']);
 
     $query = "UPDATE customer SET nama_customer = '$nama_customer', jk_customer = '$jk_customer', alamat_customer = '$alamat_customer', email_customer = '$email_customer', telp_customer = '$telp_customer' WHERE id_customer = '$id_customer'";
 
