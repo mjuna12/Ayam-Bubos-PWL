a6af7ff01880cc4c85e8eccb553e4d276a84c3e6
David Galih Pradana <62248505+DavidGalih00@users.noreply.github.com>
2022-01-28 17:34:06 +0700

    fp-pwl

--- admin/pages/tables/transaksi/table_transaksi.php
+++ admin/pages/tables/transaksi/table_transaksi.php
@@ -2,40 +2,7 @@
 include '../../../../koneksi/connection.php';
 ?>
 
-<?php 
-session_start();
-if(!$_SESSION['username'] && !$_SESSION['password'] && $_SESSION['tipe_user'] != "Admin")
-{
-    echo "
-		<script type='text/javascript'>
-		alert('Anda harus login terlebih dahulu!')
-		window.location='../../../index.php';
-		</script>";
-}
-else
-{
-?>
-
-<!doctype html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="../../../assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link href="../../../assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../../assets/libs/css/style.css">
-    <link rel="stylesheet" href="../../../assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
-    <link rel="stylesheet" href="../../../assets/vendor/charts/chartist-bundle/chartist.css">
-    <link rel="stylesheet" href="../../../assets/vendor/charts/morris-bundle/morris.css">
-    <link rel="stylesheet" href="../../../assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="../../../assets/vendor/charts/c3charts/c3.css">
-    <link rel="stylesheet" href="../../../assets/vendor/fonts/flag-icon-css/flag-icon.min.css">
-    <link href='../../../../images/logo.png' rel='SHORTCUT ICON' />
-    <title>Admin | Ayam Bubos</title>
-</head>
+<?php include('../../../partials/headertrans.php'); ?>
 
 <body>
     <div class="dashboard-main-wrapper">
